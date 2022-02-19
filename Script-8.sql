e3dca27a22070be14b09e39fcf5ad39fc9a51447
valwork <98618139+valwork@users.noreply.github.com>
2022-02-19 14:21:53 +0300

    Add files via upload

---
+++ Script-8.sql
@@ -0,0 +1,112 @@
+1 +
+select city ->> 'ru' as "Города"
+from airports_data ad 
+group by city having count (airport_code) > 1;
+
+2 +
+select distinct f.arrival_airport as "Аэропорт" from (
+select ad.aircraft_code, max (ad.range)
+from aircrafts_data ad 
+group by ad.aircraft_code
+order by max (ad.range)
+desc limit 1
+) mr
+join flights f on f.aircraft_code = mr.aircraft_code;
+
+3 +
+select flight_id, flight_no 
+from flights_v fv 
+where actual_duration > scheduled_duration 
+order by actual_duration - scheduled_duration 
+desc limit 10;
+
+4 +
+select distinct t.book_ref
+from tickets t 
+left join boarding_passes bp on t.ticket_no = bp.ticket_no 
+where bp.ticket_no is null;
+
+5 +
+
+with c1 as
+(select distinct bp.flight_id, count(s.seat_no) as c1
+from seats s 
+join boarding_passes bp on bp.seat_no = s.seat_no 
+group by bp.flight_id),
+ c2 as 
+(select distinct bp.flight_id, count(bp.seat_no) as c2
+from boarding_passes bp
+group by bp.flight_id),
+c3 as (select c1.flight_id, (c1.c1 - c2.c2) as c3
+from c2
+join c1 on c2.flight_id = c1.flight_id
+)
+select c1.flight_id, c3.c3 "свободные", 
+round(c3.c3::numeric / c1.c1::numeric, 3) * 100 as "процент",
+f.actual_departure::date,
+f.departure_airport,
+count (bp.ticket_no) over (partition by f.departure_airport order by f.actual_departure::date)
+as "накопительный итог"
+from c3
+join c1 on c3.flight_id = c1.flight_id
+join boarding_passes bp on c1.flight_id = bp.flight_id 
+join flights f on bp.flight_id = f.flight_id 
+
+
+6 +
+
+select fa.aircraft_code, round (fa.c1, 4) * 100 as "процент"
+from (
+select ad.aircraft_code, 
+count (f.flight_id)::numeric/(select count(flight_id)::numeric from flights f) as c1
+from flights f
+right join aircrafts_data ad on f.aircraft_code = ad.aircraft_code
+group by ad.aircraft_code 
+) fa
+
+
+
+
+7 -
+
+with c1 as (
+select tf.flight_id, tf.amount as a1
+from ticket_flights tf 
+where tf.fare_conditions ilike 'economy'
+),
+c2 as (
+select tf.flight_id, tf.amount as a2
+from ticket_flights tf 
+where tf.fare_conditions ilike 'business'
+), c3 as
+(select distinct tf.flight_id 
+from ticket_flights tf
+join c1 on tf.flight_id = c1.flight_id
+join c2 on tf.flight_id = c2.flight_id
+where c1.a1 > c2.a2
+group by tf.flight_id
+)
+select ad.city ->> 'ru'
+from c3
+join flights f on c3.flight_id = f.flight_id
+join airports_data ad on f.arrivar_airport = ad.airport_code 
+
+
+8 +
+
+with c1 as(
+select f.arrival_airport as faa, ad.city ->> 'ru' as ac, f.flight_id 
+from flights f 
+join airports_data ad on f.arrival_airport = ad.airport_code 
+),
+c2 as(
+select f.departure_airport as fda, ad.city ->> 'ru' as dc, f.flight_id 
+from flights f 
+join airports_data ad on f.departure_airport = ad.airport_code 
+)
+select ad.city ->> 'ru' as "city 1", ad2.city ->> 'ru' as "city 2"
+from airports_data ad, airports_data ad2 
+except
+select distinct c2.dc, c1.ac
+from c1
+join c2 on c1.flight_id = c2.flight_id
